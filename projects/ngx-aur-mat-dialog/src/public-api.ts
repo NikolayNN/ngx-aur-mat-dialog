980cb7b798c4a7fb5f904af1ba0e0743c778929b
Nikolay <nikolay.horushko@gmail.com>
2024-05-06 15:21:21 +0300

    Export sticky header and footer directives in ngx-aur-mat-dialog public API

--- projects/ngx-aur-mat-dialog/src/public-api.ts
+++ projects/ngx-aur-mat-dialog/src/public-api.ts
@@ -6,3 +6,5 @@ export * from './lib/aur-dialog.component';
 export * from './lib/aur-dialog.module';
 export * from './lib/aur-dialog-actions.directive';
 export * from './lib/aur-dialog-content.directive';
+export * from './lib/aur-dialog-sticky-header.directive';
+export * from './lib/aur-dialog-sticky-footer.directive';
